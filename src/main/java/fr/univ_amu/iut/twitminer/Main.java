2c460de711d016ea379aabdcf6f027cee3cb666c
Thomas Peyre <thomas.peyre@etu.univ-amu.fr>
2017-03-31 16:46:55 +0200

    Créé une classe twitGetter qui ajoute à resultats.csv les tweets

--- src/main/java/fr/univ_amu/iut/twitminer/Main.java
+++ src/main/java/fr/univ_amu/iut/twitminer/Main.java
@@ -1,34 +1,16 @@
 package fr.univ_amu.iut.twitminer;
 
+
 import twitter4j.*;
 
 import java.io.*;
 import java.util.List;
 
+import static fr.univ_amu.iut.twitminer.twitGetter.getTwits;
+
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            Twitter twitter = new TwitterFactory().getInstance();
-            System.out.println("Please tpye your searching expression: ");
-            BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter fichier = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("resultats.csv"))));
-            Query query = new Query(clavier.readLine());
-            QueryResult result;
-            do {
-                result = twitter.search(query);
-                List<Status> tweets = result.getTweets();
-                for (Status tweet : tweets) {
-                    fichier.write("\"" + tweet.getCreatedAt().toString() + "\";\"@" + tweet.getUser().getScreenName() + "\";\"" + tweet.getText());
-                    fichier.newLine();
-                }
-            } while ((query = result.nextQuery()) != null);
-            System.exit(0);
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to search tweets: " + te.getMessage());
-            System.exit(-1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        getTwits();
     }
 }
---
+++ src/main/java/fr/univ_amu/iut/twitminer/twitGetter.java
@@ -0,0 +1,39 @@
+package fr.univ_amu.iut.twitminer;
+
+import twitter4j.*;
+
+import java.io.*;
+import java.util.List;
+
+public class twitGetter {
+    public static void getTwits() {
+        try {
+            Twitter twitter = new TwitterFactory().getInstance();
+            BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter fichier = new BufferedWriter(new FileWriter("resultats.csv",true));
+
+            Query query = new Query(clavier.readLine());
+            query.setCount(10);
+            QueryResult result;
+
+            do {
+                result = twitter.search(query);
+                List<Status> tweets = result.getTweets();
+                for (Status tweet : tweets) {
+                    fichier.write("\"" + tweet.getCreatedAt().toString()
+                            + "\";\"@" + tweet.getUser().getScreenName()
+                            + "\";\"" +  tweet.getText().replace(" ","\";\"").replace("\n"," "));
+                    fichier.write("\";");
+                    fichier.newLine();
+                }
+            } while ((query = result.nextQuery()) != null);
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
