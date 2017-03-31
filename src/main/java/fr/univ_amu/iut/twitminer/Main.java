26691827d64350cd1f465adbac34f863a6c3ee7b
GeeksdelaCave <geeksdelacave@laposte.net>
2017-03-31 15:05:10 +0200

    Du code qui marche :D

--- src/main/java/fr/univ_amu/iut/twitminer/Main.java
+++ src/main/java/fr/univ_amu/iut/twitminer/Main.java
@@ -1,7 +1,20 @@
 package fr.univ_amu.iut.twitminer;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Salam aëlekoum world!");
+
+        Twitter twitter = TwitterFactory.getSingleton();
+        try {
+            Status status = twitter.updateStatus("Salut ! Ça va ?");
+            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 }
